ae4c531cb3679b8367f944a49a468b8eb5ca5b2c
Fagner Junqueira <58519744+fagnerjunqueira@users.noreply.github.com>
2024-03-22 21:29:04 -0300

    tela de login

---
+++ app/View/login/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Gevac</title>
+</head>
+<body>
+    <section class="login" style="height: 100%;">
+        <div class="card card-info" style="width: 300px; height: 70%;">
+            <div class="card-body">
+              <h5 class="card-title" style="font-family: 'Merriweather';font-size: 90px;">Gevac</h5>
+              <p class="card-text">Gestão de Dados sobre a Vacinação da Covid-19</p>
+            </div>
+        </div>
+
+          <div class="card" style="width: 500px; height: 70%;">
+            <div class="card-body">
+              <h5 class="card-title">Iniciar sessão</h5>
+              <h6 class="card-subtitle mb-2 text-muted">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                    </div>
+                    <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                  </div>
+
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                    </div>
+                    <input type="password" class="form-control" placeholder="Senha" aria-label="Senha" aria-describedby="basic-addon1">
+                  </div>
+              </h6>
+              <button class="btn btn-primary" type="submit">Iniciar</button>
+              <button type="button" class="btn btn-outline-secondary">Registrar</button>
+            </div>
+
+          </div>
+
+    </section>
+</body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
