b999bfcab234ffaf198e1bdf4e09b1bfed7e5553
Fagner Junqueira <58519744+fagnerjunqueira@users.noreply.github.com>
2024-03-22 21:26:53 -0300

    Base para construir novas telas

---
+++ app/View/base_to_construct_screen/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>GeVac</title>
+</head>
+<body>
+    <div class="row">
+        <div class="col-4 menu">
+            <p class="title" href="#">GeVac</p>
+            <hr>
+            <div class="buttons">
+                <button type="button" class="btn btn-lg btn-outline-success">Dashboard</button><br>
+                <button type="button" class="btn btn-lg btn-outline-success">Controle</button><br>
+                <button type="button" class="btn btn-lg btn-outline-success">Profile</button><br>
+                <button type="button" class="btn btn-lg btn-outline-success">Logout</button><br>
+            </div>
+        </div>
+        <div class="col-8 corpo">
+            <div class="topo">
+
+            </div>
+            <br>
+            <div class="baixo">
+
+            </div>
+        </div>
+      </div>
+</body>
+</html>
