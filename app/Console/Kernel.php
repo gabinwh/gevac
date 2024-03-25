6f30d2c894589229159716ce46debe645c6662db
Tarcisio Almeida <tarcisio.olv@gmail.com>
2024-03-25 16:57:12 -0300

    fix: altera frequencia da rotina

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('fetch:data')->everyMinute();
+        $schedule->command('fetch:data')->daily();
     }
 
     /**
