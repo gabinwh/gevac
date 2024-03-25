19ed424381a0dc64fb81add97fabe258370c90bd
Tarcisio Almeida <tarcisio.olv@gmail.com>
2024-03-25 16:56:09 -0300

    feat: funções para importação de dados e rotina

--- app/Http/Controllers/ImportacaoController.php
+++ app/Http/Controllers/ImportacaoController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cidadao;
-use App\Models\Dose;
-use App\Models\Endereco;
-use App\Models\Estabelecimento;
-use App\Models\Vacina;
 use Carbon\Carbon;
+use App\Models\Dose;
+use App\Models\Vacina;
+use App\Models\Cidadao;
+use App\Models\Endereco;
 use Illuminate\Http\Request;
+use App\Models\Estabelecimento;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 
 class ImportacaoController extends Controller
@@ -81,6 +82,74 @@ class ImportacaoController extends Controller
             return response()->json(['success' => 'Sucesso ao importar os dados.']);
         } else {
             return response()->json(['error' => 'Erro ao acessar os dados.']);
+        }
+    }
+
+    public function scheduleFetchData()
+    {
+        try {
+            $url = 'https://imunizacao-es.saude.gov.br/desc-imunizacao/_search?q=estabelecimento_uf:SE';
+
+            $response = Http::withOptions(['verify' => false])
+                ->withBasicAuth('imunizacao_public', 'qlto5t&7r_@+#Tlstigi')
+                ->get($url);
+
+            if ($response->successful()) {
+                $data = $response->json();
+
+                foreach ($data['hits']['hits'] as $dose) {
+
+                    $endereco = Endereco::firstOrCreate([
+                        'uf' => $dose['_source']['paciente_endereco_uf'],
+                        'codigo_pais' => $dose['_source']['paciente_endereco_coPais'],
+                        'codigo_ibge_municipio' => $dose['_source']['paciente_endereco_coIbgeMunicipio'],
+                        'municipio_nome' => $dose['_source']['paciente_endereco_nmMunicipio'],
+                        'pais_nome' => $dose['_source']['paciente_endereco_nmPais'],
+                        'cep' => $dose['_source']['paciente_endereco_cep']
+                    ]);
+
+                    $cidadao = Cidadao::firstOrCreate([
+                        'data_nascimento' => $dose['_source']['paciente_dataNascimento'],
+                        'codigo_raca_cor' => $dose['_source']['paciente_racaCor_codigo'],
+                        'codigo_nacionalidade' => $dose['_source']['paciente_nacionalidade_enumNacionalidade'],
+                        'sexo_biologico' => $dose['_source']['paciente_enumSexoBiologico'],
+                        'endereco_id' => $endereco->id
+                    ]);
+
+                    $estabelecimento = Estabelecimento::firstOrCreate([
+                        'cnes_valor' => $dose['_source']['estabelecimento_valor'],
+                        'razao_social' => $dose['_source']['estabelecimento_razaoSocial'],
+                        'nome_fantasia' => $dose['_source']['estalecimento_noFantasia'],
+                        'codigo_municipio' => $dose['_source']['estabelecimento_municipio_codigo'],
+                        'municipio' => $dose['_source']['estabelecimento_municipio_nome'],
+                        'uf' => $dose['_source']['estabelecimento_uf']
+                    ]);
+
+                    $vacina = Vacina::firstOrCreate([
+                        'nome' => $dose['_source']['vacina_nome'],
+                        'codigo' => $dose['_source']['vacina_codigo'],
+                        'fabricante' => $dose['_source']['vacina_fabricante_nome'],
+                        'cnpj_fabricante' => $dose['_source']['vacina_fabricante_referencia'],
+                    ]);
+
+                    $dose = Dose::create([
+                        'cidadao_id' => $cidadao->id,
+                        'vacina_id' => $vacina->id,
+                        'estabelecimento_id' => $estabelecimento->id,
+                        'grupo_atendimento' => $dose['_source']['vacina_grupoAtendimento_nome'],
+                        'grupo_atendimento_codigo' => $dose['_source']['vacina_grupoAtendimento_nome'],
+                        'categoria' => $dose['_source']['vacina_categoria_nome'],
+                        'categoria_codigo' => $dose['_source']['vacina_categoria_codigo'],
+                        'lote' => $dose['_source']['vacina_lote'],
+                        'data_aplicacao' => Carbon::create($dose['_source']['vacina_dataAplicacao']),
+                        'descricao_dose' => $dose['_source']['vacina_descricao_dose'],
+                    ]);
+                }
+            } else {
+                Log::error('Falha ao buscar dados da API: ' . $response->status());
+            }
+        } catch (\Exception $e) {
+            Log::error('Ocorreu uma exceção durante a busca de dados: ' . $e->getMessage());
         }
     }
 }
